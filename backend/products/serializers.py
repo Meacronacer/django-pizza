2517649d8d6a2d0fa044a8f16784721fcd58ce22
John <65309726+Meacronacer@users.noreply.github.com>
2024-08-17 07:54:11 +0300

    Update serializers.py

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -21,54 +21,10 @@ class IngredientsSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     name = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    sizes = serializers.SerializerMethodField()
-    ingredients = serializers.SerializerMethodField()
+    sizes = SizesSerializer(many=True, read_only=True)
+    ingredients = IngredientsSerializer(many=True, read_only=True)
 
-    def get_sizes(self, obj):
-        selected_options = Sizes.objects.filter(name__name=obj.name)
-        return SizesSerializer(selected_options, many=True).data
-    
-    def get_ingredients(self, obj):
-        return IngredientsSerializer(obj.ingredients, many=True).data
     
     class Meta:
         model = Product
-        fields = '__all__'
-
-
-# class ProductGroupSerializer(serializers.ModelSerializer):
-#     snacks = serializers.SerializerMethodField()
-#     # beverages = serializers.SerializerMethodField()
-#     # cocktails = serializers.SerializerMethodField()
-#     # cofe = serializers.SerializerMethodField()
-#     # deserts = serializers.SerializerMethodField()
-#     # sauces = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = Product
-#         fields = ('snacks', )
-
-#     def _build_account_list(self, product_type):
-#         get_product = Product.objects.filter(product_type=product_type)
-#         serializer = ProductSerializer(get_product, many=True)
-#         return serializer.data
-
-#     def get_snacks(self, obj):
-#         return self._build_account_list(Product.SNACKS)
-    
-#     # def get_beverages(self, obj):
-#     #     return self._build_account_list(Product.BEVERAGES)
-    
-#     # def get_cocktails(self, obj):
-#     #     return self._build_account_list(Product.COCKTAILS)
-    
-#     # def get_cofe(self, obj):
-#     #     return self._build_account_list(Product.COFE)
-    
-#     # def get_deserts(self, obj):
-#     #     return self._build_account_list(Product.DESERTS)
-    
-#     # def get_sauces(self, obj):
-#     #     return self._build_account_list(Product.SAUCES)
-    
-
+        fields = ['id','name', 'product_type', 'sizes', 'ingredients', 'img_url', 'description']
